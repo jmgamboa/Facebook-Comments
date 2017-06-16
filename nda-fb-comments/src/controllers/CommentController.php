51888a9d5441b011b8124309a9e0acb3924ff8ef
Joaquin Gamboa <gamboanj@gmail.com>
2017-06-16 00:02:35 -0400

    Update CommentController.php

--- nda-fb-comments/src/controllers/CommentController.php
+++ nda-fb-comments/src/controllers/CommentController.php
@@ -3,7 +3,7 @@
 class CommentController{
 
   // save as wordpress comments
-  function save_new_comment($fbcomment){
+  function save_new_comment($fbcomment) {
     global $wpdb;
     $duplicate = $this->check_duplicate($fbcomment);
     if (!$duplicate){
@@ -27,7 +27,7 @@ class CommentController{
   }
   
   // save meta properties according to primary id of the last inserted comment
-  function save_meta_fbid($fbcomment, $last_id){
+  function save_meta_fbid($fbcomment, $last_id) {
     global $wpdb;
     $wpdb->insert("wp_commentmeta", array(
        'comment_id' => $last_id,
@@ -35,8 +35,9 @@ class CommentController{
        'meta_value' => $fbcomment->meta_fbid
       ));
   }
+	
   // save meta property fb user id according to primary id of the last inserted comment
-  function save_meta_user($fbcomment, $last_id){
+  function save_meta_user($fbcomment, $last_id) {
     global $wpdb;
     $wpdb->insert("wp_commentmeta", array(
        'comment_id' => $last_id,
@@ -44,8 +45,9 @@ class CommentController{
        'meta_value' => $fbcomment->meta_fb_user_id
       ));
   }
+	
   // check for duplicate by getting count of results
-  function check_duplicate($fbcomment){
+  function check_duplicate($fbcomment) {
     global $wpdb;
     $fbid = $fbcomment->meta_fbid;
     $results = $wpdb->get_var("SELECT COUNT(*) FROM wp_commentmeta WHERE meta_key='fb_comment_id' AND meta_value='$fbid'");
@@ -53,7 +55,7 @@ class CommentController{
   }
 
 	// get response and format into php obj
-	function curl_and_jdecode($endpoint){
+	function curl_and_jdecode($endpoint) {
 		// Initiate curl
 		$ch = curl_init();
 		// Disable SSL verification
@@ -71,33 +73,33 @@ class CommentController{
 		return $fb_comments_obj;
 	}
 
-  function get_fbids_comment($postid){
-      global $wpdb;
-      $fbids = $wpdb->get_results("SELECT wp_commentmeta.meta_value, wp_commentmeta.comment_id
-                                    FROM wp_comments
-                                    JOIN wp_commentmeta
-                                    ON wp_comments.comment_ID = wp_commentmeta.comment_id
-                                    WHERE comment_post_ID = '$postid'
-                                    AND  wp_commentmeta.meta_key = 'fb_comment_id';");
-      return $fbids;
-  	}
+	function get_fbids_comment($postid) {
+            global $wpdb;
+	    $fbids = $wpdb->get_results("SELECT wp_commentmeta.meta_value, wp_commentmeta.comment_id
+					 FROM wp_comments
+					 JOIN wp_commentmeta
+					 ON wp_comments.comment_ID = wp_commentmeta.comment_id
+					 WHERE comment_post_ID = '$postid'
+					 AND  wp_commentmeta.meta_key = 'fb_comment_id';");
+            return $fbids;
+	}
 
-  function update_reply($fbcomment_id, $wp_comment_id){
-    global $wpdb;
-    $wpdb->query("UPDATE wp_comments 
-      JOIN wp_commentmeta
-      ON wp_comments.comment_ID = wp_commentmeta.comment_id
-      SET wp_comments.comment_parent = '$wp_comment_id'
-      WHERE wp_commentmeta.meta_key = 'fb_comment_id'
-      AND wp_commentmeta.meta_value = '$fbcomment_id';");
-  	}
+	function update_reply($fbcomment_id, $wp_comment_id) {
+	    global $wpdb;
+	    $wpdb->query("UPDATE wp_comments 
+	                 JOIN wp_commentmeta
+	                 ON wp_comments.comment_ID = wp_commentmeta.comment_id
+	                 SET wp_comments.comment_parent = '$wp_comment_id'
+	                 WHERE wp_commentmeta.meta_key = 'fb_comment_id'
+	                 AND wp_commentmeta.meta_value = '$fbcomment_id';");
+        }
 
 	// Get ids depending on batch
-	function get_smaller_post($postid=null){
-		global $wpdb;
-		if ($postid){
+	function get_smaller_post($postid=null) {
+	    global $wpdb;
+	    if ($postid) {
 			$pid = $wpdb->get_results("SELECT ID FROM `wp_posts` WHERE  post_status='publish' AND post_type='post' AND ID < '$postid' ORDER BY ID DESC LIMIT 1");
-		} else{
+		} else {
 			$pid = $wpdb->get_results("SELECT ID FROM `wp_posts` WHERE  post_status='publish' AND post_type='post' ORDER BY ID DESC LIMIT 1");
 		}
 		$postid = $pid[0];
